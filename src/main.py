e11fdce236336b8a11c1fecd844ec966829fe82c
MovingJu <baramishe@gmail.com>
2025-11-22 17:37:08 +0900

    add something

--- src/main.py
+++ src/main.py
@@ -12,3 +12,10 @@ app.include_router(routes.login.router)
 async def read_root():
     return {"message": "Hello, World!"}
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8080
+    )
